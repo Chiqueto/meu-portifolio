046867b1d412f28f8bd4b49c2d78e5784e9d6364
Chiqueto <lfchiqueto@gmail.com>
2024-09-03 22:14:26 -0300

    feat: add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div>
-      <p className="text-lime-400">Olá</p>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+const Header = () => {
+  return (
+    <header className="w-full font-header py-8 bg-header">
+      <img src="" alt="" />
+      <ul className="font-semibold text-zinc-50 text-sm flex flex-row gap-16 justify-center items-center">
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Projetos</a>
+        </li>
+        <li>
+          <a href="">Experiência</a>
+        </li>
+        <li>
+          <a href="">Contato</a>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
