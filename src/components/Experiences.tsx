8b470aef10fc99aa0c13619d55fbe9a9aaac2b4a
chiqueto <lfchiqueto@gmail.com>
2024-09-04 16:37:56 -0300

    feat: add Experience component

---
+++ src/components/Experiences.tsx
@@ -0,0 +1,14 @@
+const Experiences = () => {
+  return (
+    <section>
+      <h2 className="uppercase text-zinc-300 text-center font-semibold text-xl">
+        Experiência com
+      </h2>
+      <ul>
+        <li></li>
+      </ul>
+    </section>
+  );
+};
+
+export default Experiences;
