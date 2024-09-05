690ff9713a1b9737744ca91b2da2c6381b499abe
chiqueto <lfchiqueto@gmail.com>
2024-09-05 14:08:19 -0300

    feat: add Projects component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Experiences from "./components/Experiences";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Home />
       <Experiences />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.tsx
@@ -0,0 +1,22 @@
+const Projects = () => {
+  return (
+    <section className="mt-24 flex flex-col justify-center items-center mx-auto">
+      <h1 className="font-extrabold text-4xl bg-gradient-to-t to-[#FF8660]  from-[#D5491D] bg-clip-text text-transparent">
+        Projetos
+      </h1>
+      <div>
+        <div className="w-96">
+          <img src="" alt="" className="h-60" />
+          <div>
+            <div>
+              <p>Clique aqui para visitar</p>
+              <p>HTML Tutorial</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
