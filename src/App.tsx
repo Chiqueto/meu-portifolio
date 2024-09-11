8c318df8cb0cedd1a23fe77109c8cef80357d0ec
chiqueto <lfchiqueto@gmail.com>
2024-09-11 08:05:17 -0300

    feat: embbed video

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,10 @@ function App() {
       <Projects />
       <Experience />
       <Contact />
-      <div className="bg-neutral-800 px-5 py-4 max-w-screen-sm mx-5">
+      <div className="bg-neutral-800 max-w-fit px-5 py-4 -sm mx-auto rounded-2xl">
         <iframe
+          className="mx-auto w-[320px] h-[180px] sm:w-[720px] sm:h-[405px]"
           src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7239307519758663680?compact=1"
-          frameborder="0"
-          allowfullscreen=""
           title="Publicação incorporada"
         ></iframe>
       </div>
