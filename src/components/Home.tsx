c45600ca569d23117e67d225925055d0b65a7332
chiqueto <lfchiqueto@gmail.com>
2024-10-21 16:38:11 -0300

    feat: alterei a forma de abertura do CV

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -63,9 +63,10 @@ const Home = () => {
         </button>
         <a
           className="border-2 border-zinc-300 text-zinc-300 py-4 sm:px-6 px-3  font-semibold text-lg rounded-full  hover:text-zinc-50 hover:border-zinc-50 text-center"
-          href="https://docs.google.com/document/d/14DDBhwwm5wf76eeB61CwlZoVzkjAIhEaMHf0VqzCuCc/export?format=pdf
-"
+          href="https://docs.google.com/document/d/14DDBhwwm5wf76eeB61CwlZoVzkjAIhEaMHf0VqzCuCc/view
+" //https://docs.google.com/document/d/14DDBhwwm5wf76eeB61CwlZoVzkjAIhEaMHf0VqzCuCc/export?format=pdf
           download={"Luis Felipe Chiqueto - Curriculo.pdf"}
+          target="_blank"
         >
           Download CV
         </a>
